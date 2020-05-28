5d897c8610df101adf8d29170a0bb08556dbc149
Roy Reyna <royreyna7@yahoo.com>
2020-05-28 00:32:02 -0500

    scrape off NYT working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,51 @@
 //Initialize express and router
 var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
 
 //Set up dependencies
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 //Routes
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
     res.send("Congrats! You're connected!")
 });
 
-router.get("/scrape", function(req, res){
+router.get("/scrape", function (req, res) {
+    axios.get("https://www.nytimes.com/")
+        .then(function (response) {
+
+            var $ = cheerio.load(response.data);
+            var articles = [];
+
+            $("div.css-6p6lnl").each(function(i, element){
+               
+                var result = {};
+
+                result.link = $(element).children("a").attr("href");
+
+                let title = $(element).children().children().children("h2").text();
+                if(!title){
+                    let name = $(element).children().children().children().children("span").text();
+                    result.title = name;
+                } else {
+                    result.title = title;
+                }
+
+                articles.push(result);
+            })
+
+            console.log(articles);
+            res.send("Scrape Complete!");
+        })
+});
+
+router.get("/saved", function (req, res) {
 
 });
 
-router.get("/saved", function(req, res){
-
-});
-
-router.post("/saved/:id", function(req, res){
+router.post("/saved/:id", function (req, res) {
 
 })
 
